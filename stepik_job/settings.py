7dbeac22d2dbf6abae77d0b4169c4ef7176f9a78
OlegVR <olegrezvov89@gmail.com>
2020-06-30 22:11:14 +0300

    remove settings

--- stepik_job/settings.py
+++ stepik_job/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6*c^gzse^xzv)*i(&=i9e56&xn-%)i!lg_!#5=9(cizq2*-cq-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
